cce35c1c2c664bc4774ac26de133aa073872871f
Freddy Ponce <fponce@espol.edu.ec>
2025-06-24 20:44:26 -0500

    Adding coach details view

---
+++ src/pages/CoachDetails.jsx
@@ -0,0 +1,17 @@
+import { Navbar } from '../components/Navbar'
+import { SubBar } from '../components/SubBar'
+import InfoCoachSection from '../components/InfoCoachSection'
+import ReviewsTable from '../components/ReviewsTable'
+
+function CoachDetails() {
+  return (
+    <>
+      <Navbar />
+      <SubBar />
+      <InfoCoachSection />
+      <ReviewsTable />
+    </>
+  )
+}
+
+export default CoachDetails
