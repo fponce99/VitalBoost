22baaf1ac13646fb658e9fd3d2176f23563cf014
Freddy Ponce <fponce@espol.edu.ec>
2025-06-24 20:45:18 -0500

    Adding Mainviewcoach

---
+++ src/pages/MainViewCoach.jsx
@@ -0,0 +1,15 @@
+import { Navbar } from '../components/Navbar'
+import { SubBar } from '../components/SubBar'
+import ResultTableReserves from '../components/ResultTableReserves';
+
+function MainViewCoach() {
+  return (
+    <>
+      <Navbar />
+      <SubBar />
+      <ResultTableReserves />
+    </>
+  );
+}
+
+export default MainViewCoach;
